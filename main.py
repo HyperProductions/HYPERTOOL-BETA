3481192e6c3fe2407ab0b85713f6b4437b29aa52
HyperProductions <gulerege129@gmail.com>
2026-05-14 20:33:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ GRAY = "\033[90m"
 RED = "\033[91m"
 GREEN = "\033[92m"
 
-DEBUG_KEY = "h*23465245-V2"
+DEBUG_KEY = "h*23465245-v2"
 debug_mode = False
+
 
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
+
 
 def banner():
     print(MOR + BOLD)
@@ -31,21 +33,26 @@ def banner():
     """)
     print(RESET)
 
+
 def wait():
     input(GRAY + "\nPress Enter to continue..." + RESET)
 
+
 def iplogger():
     print("IP LOGGER System...")
+    print("Opening browser...")
     webbrowser.open("https://iplogger.org")
     wait()
 
+
 def debug_panel():
-    print(GREEN + "\n[DEBUG MODE]" + RESET)
+    print(GREEN + "\n[ DEBUG PANEL ]\n" + RESET)
 
     files = os.listdir()
 
     for file in files:
         print(GRAY + file + RESET)
+
 
 def menu():
     global debug_mode
@@ -67,16 +74,25 @@ def menu():
 
         print(RED + "[0]" + RESET + " Exit")
 
-        choice = input("\n>> Select: ").strip()
+        try:
+            choice = input("\n>> Select: ").strip().lower()
 
-        # SECRET DEBUG MODE
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            sys.exit()
+
+        # SECRET DEBUG KEY
         if choice == DEBUG_KEY:h*23465245-v2
             debug_mode = not debug_mode
-            print(GREEN + "\nDEBUG MODE TOGGLED!" + RESET)
+
+            print(GREEN + f"\nDEBUG MODE: {debug_mode}" + RESET)
+
             wait()
             continue
 
+        # DEBUG PANEL
         if debug_mode and choice == "debug":
+            clear()
             debug_panel()
             wait()
             continue
@@ -112,6 +128,7 @@ def menu():
             print(RED + "Invalid option!" + RESET)
 
         wait()
+
 
 if __name__ == "__main__":
     menu()
