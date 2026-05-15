db089df66c9dba869d66a0c0e7f75246f19bcf8d
HyperProductions <gulerege129@gmail.com>
2026-05-15 12:49:50 +0300

    Delete modules directory

--- modules/iplogger.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-def run():
-    print("IP Logger system...")
-    print("Opening IP Logger...")
-    webbrowser.open("https://iplogger.org")
-    input("https://iplogger.org")
--- modules/systeminfo.py
+++
@@ -1,9 +0,0 @@
-import platform
-import os
-
-def run():
-    print("\n=== SYSTEM INFO ===\n")
-    print("OS:", platform.system())
-    print("Version:", platform.version())
-    print("Machine:", platform.machine())
-    print("Processor:", platform.processor())
